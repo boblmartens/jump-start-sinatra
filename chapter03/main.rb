ca75aad2ceea4603282eeec25be5879a9a118caf
Bob Martens <martenrt@mlc-wels.edu>
2015-04-20 10:09:14 -0500

    individual song pages complete

---
+++ chapter03/main.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'slim'
+require 'sass'
+
+require './song'
+
+get('/styles.css'){scss :styles}
+
+get '/' do
+  @title = "There's No Place Like Home"
+  slim :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do 
+  @title = "Smooth As Silk"
+  slim :contact
+end
+
+not_found do
+  slim :not_found
+end
+
+get '/songs' do
+  @songs = Song.all
+  slim :songs
+end
+
+get '/songs/:id' do
+  @song = Song.get(params[:id])
+  slim :show_song
+end
